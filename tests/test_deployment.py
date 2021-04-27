26f53135cea3d128570158aacbcedf3523ca485b
cortadocodes <cortado.codes@protonmail.com>
2021-04-27 21:08:18 +0100

    TST: Add deployment test

---
+++ tests/test_deployment.py
@@ -0,0 +1,29 @@
+import os
+from unittest import TestCase
+
+from octue.cloud.pub_sub.service import Service
+from octue.resources.service_backends import GCPPubSubBackend
+
+
+class TestDeployment(TestCase):
+
+    def test_deployment(self):
+        """Test that the Google Cloud Run integration works, providing a service that can be asked questions and send
+        responses.
+        """
+        service_id = "octue.services.8765b5f2-1778-4a6f-809d-46b91e01bd97"
+        asker = Service(backend=GCPPubSubBackend(project_name=os.environ["TEST_PROJECT_NAME"]))
+
+        input_values = {
+            "alpha_range": [-10, 10, 5],
+            "inflow_speed": 1,
+            "kinematic_viscosity": 1e-6,
+            "characteristic_length": 1e-6,
+            "mach_number": 0,
+            "n_critical": 9,
+            "re_xtr": [5e5, 5e5]
+        }
+
+        subscription, _ = asker.ask(service_id=service_id, input_values=input_values)
+        answer = asker.wait_for_answer(subscription)
+        self.assertEqual(answer, {"output_values": [1, 2, 3, 4, 5], "output_manifest": None})
