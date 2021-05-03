9a0aa569bc9ff066f8653dd77effe0b59e8205e3
cortadocodes <cortado.codes@protonmail.com>
2021-05-03 16:08:43 +0100

    OPS: Add version to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
-import os
 from setuptools import setup
-
-
-def git_version():
-    return os.system('git rev-parse HEAD')
 
 
 setup(
     name="panel-codes-twine",
-    version=git_version(),
+    version="0.0.0",
     py_modules=['app'],
     entry_points='''
     [console_scripts]
