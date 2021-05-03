4159890394859dd4b87e6a20132c2afebe6ae90d
cortadocodes <cortado.codes@protonmail.com>
2021-05-03 14:11:19 +0100

    TST: Use absolute path for test cases

--- tests/xfoil/test_xfoil.py
+++ tests/xfoil/test_xfoil.py
@@ -1,7 +1,5 @@
-import json
 import os
-import tempfile
-from unittest import TestCase, mock
+from unittest import TestCase
 
 from octue import Runner
 
@@ -9,28 +7,26 @@ from app import REPOSITORY_ROOT
 
 
 class TestXFoil(TestCase):
-    def setUp(self):
-        super().setUp()
-        self.start_path = os.getcwd()
-        self.case_path = None
 
-        def set_xfoil_case(xfoil_case):
-            """Sets XFOIL with the case"""
-            self.case_path=os.path.join("cases", xfoil_case)
-
-        self.set_xfoil_case = set_xfoil_case
+    def get_xfoil_case_path(self, xfoil_case):
+        """Sets XFOIL with the case"""
+        return os.path.join(REPOSITORY_ROOT, "tests", "xfoil", "cases", xfoil_case)
 
     def test_call(self):
+        """Test that xfoil runs NACA0012 analysis and that results are consistent with compiled version from NASA
+        website.
         """
-        Test that xfoil runs NACA0012 analysis and that results are consistent with compiled version from NASA website
-        """
-        self.set_xfoil_case("naca0012")
-        runner=Runner(
+        case_path = self.get_xfoil_case_path("naca0012")
+
+        runner = Runner(
             app_src=REPOSITORY_ROOT,
             twine=os.path.join(REPOSITORY_ROOT, "twine.json"),
-            configuration_values=os.path.join(self.case_path, "data", "configuration", "values.json")
+            configuration_values=os.path.join(case_path, "data", "configuration", "values.json")
         )
-        analysis = runner.run(input_values=os.path.join(self.case_path, "data", "input", "values.json"),
-                              input_manifest=os.path.join(self.case_path, "data", "input", "manifest.json"))
-        analysis.finalise(output_dir=os.path.join(self.case_path, "data", "output"))
 
+        analysis = runner.run(
+            input_values=os.path.join(case_path, "data", "input", "values.json"),
+            input_manifest=os.path.join(case_path, "data", "input", "manifest.json")
+        )
+
+        analysis.finalise(output_dir=os.path.join(case_path, "data", "output"))
