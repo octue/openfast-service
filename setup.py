1ec6b1fda974bf81e7e3f698d66e21bf62a7e9fc
cortadocodes <cortado.codes@protonmail.com>
2021-08-18 16:44:04 +0100

    DEP: Use latest octue version for fixed error forwarding

--- setup.py
+++ setup.py
@@ -5,12 +5,7 @@ setup(
     name="openfast-service",
     version="0.0.0",
     py_modules=['app'],
-    entry_points='''
-    [console_scripts]
-    octue-app=app:octue_app
-    ''',
     install_requires=[
-        "numpy",
-        "octue==0.2.9",
+        "octue @ https://github.com/octue/octue-sdk-python/archive/fix/format-service-error-messages-correctly.zip",
     ]
 )
