55a08a637c1991fbf4c2fbc97441db939cd006be
cortadocodes <cortado.codes@protonmail.com>
2021-10-06 15:36:06 +0100

    TST: Update deployment test

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from octue.cloud.pub_sub.service import Service
+from octue.resources import Dataset, Manifest
 from octue.resources.service_backends import GCPPubSubBackend
 
 
@@ -15,16 +16,25 @@ class TestDeployment(unittest.TestCase):
         responses. Datasets from Google Cloud Storage are used for this test.
         """
         PROJECT_NAME = os.environ["TEST_PROJECT_NAME"]
+        BUCKET_NAME = "openfast-data"
         SERVICE_ID = "octue.services.c32f9dbd-7ffb-48b1-8be5-a64495a71873"
 
-        input_values = {
-            "inflow": {"wind_speed": 1, "reference_height": 200},
-            "blade_polars": [{"section_radius": 0.05, "section_polar": [0, 0, 0]}],
-            "monitoring_radii": [0.05],
-            "model_case": "5MW_Land_DLL_WTurb/5MW_Land_DLL_WTurb.fst",
-        }
+        datasets = [
+            Dataset.from_cloud(project_name=PROJECT_NAME, cloud_path=f"gs://{BUCKET_NAME}/cloud_files/openfast"),
+            Dataset.from_cloud(project_name=PROJECT_NAME, cloud_path=f"gs://{BUCKET_NAME}/cloud_files/aero"),
+            Dataset.from_cloud(project_name=PROJECT_NAME, cloud_path=f"gs://{BUCKET_NAME}/cloud_files/beamdyn"),
+            Dataset.from_cloud(project_name=PROJECT_NAME, cloud_path=f"gs://{BUCKET_NAME}/cloud_files/elastodyn"),
+            Dataset.from_cloud(project_name=PROJECT_NAME, cloud_path=f"gs://{BUCKET_NAME}/cloud_files/inflow"),
+            Dataset.from_cloud(project_name=PROJECT_NAME, cloud_path=f"gs://{BUCKET_NAME}/cloud_files/servo"),
+            Dataset.from_cloud(project_name=PROJECT_NAME, cloud_path=f"gs://{BUCKET_NAME}/cloud_files/turbsim"),
+        ]
+
+        input_manifest = Manifest(
+            datasets=datasets,
+            keys={"openfast": 0, "aero": 1, "beamdyn": 2, "elastodyn": 3, "inflow": 4, "servo": 5, "turbsim": 6},
+        )
 
         asker = Service(backend=GCPPubSubBackend(project_name=PROJECT_NAME))
-        subscription, _ = asker.ask(service_id=SERVICE_ID, input_values=input_values)
+        subscription, _ = asker.ask(service_id=SERVICE_ID, input_manifest=input_manifest)
         answer = asker.wait_for_answer(subscription, timeout=10)
         self.assertEqual(len(answer["output_values"]), 18)
