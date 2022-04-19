4eab8a0b19b62a7a572b994472ba7932c5f39583
cortadocodes <cortado.codes@protonmail.com>
2022-04-19 16:02:45 +0100

    TST: Get children properly and mock openfast analysis properly

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,29 +1,23 @@
+import json
 import os
 import unittest
+from pathlib import Path
 from unittest.mock import patch
 
 from octue import Runner
 from octue.log_handlers import apply_log_handler
-from octue.resources import Dataset, Manifest
+from octue.resources import Manifest
 
 from openfast import REPOSITORY_ROOT
 
 
-apply_log_handler()
-
-
 TWINE_PATH = os.path.join(REPOSITORY_ROOT, "twine.json")
 
-CHILDREN = [
-    {
-        "key": "turbsim",
-        "id": "octue.services.c3b47b47-cdfa-433d-b5a8-47a58f3bf7cb",
-        "backend": {
-            "name": "GCPPubSubBackend",
-            "project_name": "aerosense-twined",
-        },
-    }
-]
+
+apply_log_handler()
+
+with open(os.path.join(REPOSITORY_ROOT, "app_configuration.json")) as f:
+    CHILDREN = json.load(f)["children"]
 
 
 class TestApp(unittest.TestCase):
@@ -33,29 +27,16 @@ class TestApp(unittest.TestCase):
         """
         BUCKET_NAME = "openfast-data"
         dataset_names = ("openfast", "aerodyn", "beamdyn", "elastodyn", "inflow", "servodyn", "turbsim")
-
         input_manifest = Manifest(datasets={name: f"gs://{BUCKET_NAME}/cloud_files/{name}" for name in dataset_names})
 
         runner = Runner(app_src=REPOSITORY_ROOT, twine=TWINE_PATH, children=CHILDREN)
 
-        mock_turbsim_output_manifest = Manifest(datasets={"openfast": f"gs://{BUCKET_NAME}/openfast"})
-
-        def mock_download_dataset(dataset, local_directory):
-            """Substitute downloading files from the cloud for using local test fixtures."""
-            for datafile in dataset:
-                datafile._local_path = os.path.join(
-                    REPOSITORY_ROOT, "data", "input", *datafile.path.split("openfast-data/")[-1].split(os.path.sep)
-                )
-
-        Dataset.download_all_files = mock_download_dataset
-
-        with patch("octue.resources.dataset.Dataset", Dataset):
-            with patch(
-                "octue.resources.child.Child.ask",
-                return_value={"output_values": None, "output_manifest": mock_turbsim_output_manifest},
-            ):
-                with patch("openfast.routines.run_subprocess_and_log_stdout_and_stderr"):
-                    analysis = runner.run(input_manifest=input_manifest.serialise())
+        # Mock running an OpenFAST analysis by creating an empty output file.
+        with patch(
+            "openfast.routines.run_subprocess_and_log_stdout_and_stderr",
+            lambda command, logger: Path(os.path.splitext(command[1])[0] + ".out").touch(),
+        ):
+            analysis = runner.run(input_manifest=input_manifest.serialise())
 
         self.assertEqual(len(analysis.output_manifest.datasets), 1)
         self.assertEqual(len(analysis.output_manifest.datasets["openfast"].files), 1)
