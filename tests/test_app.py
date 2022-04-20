cf1ece276ab36ec3ce64fb2fe38945e366a6e0a8
cortadocodes <cortado.codes@protonmail.com>
2022-04-20 15:30:35 +0100

    TST: Improve app test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,6 @@
 import json
 import os
 import unittest
-from pathlib import Path
 from unittest.mock import patch
 
 from octue import Runner
@@ -22,8 +21,8 @@ with open(os.path.join(REPOSITORY_ROOT, "app_configuration.json")) as f:
 
 class TestApp(unittest.TestCase):
     def test_app(self):
-        """Test that the app takes in input in the correct format and returns an analysis with the correct output
-        values.
+        """Test that the app takes in an input manifest of openfast files, uploads the output dataset to the cloud, and
+        returns an output manifest with a signed URL to the dataset.
         """
         dataset_names = ("openfast", "aerodyn", "beamdyn", "elastodyn", "inflow", "servodyn", "turbsim")
         input_manifest = Manifest(datasets={name: f"gs://openfast-aventa/testing/{name}" for name in dataset_names})
@@ -31,11 +30,24 @@ class TestApp(unittest.TestCase):
         runner = Runner(app_src=REPOSITORY_ROOT, twine=TWINE_PATH, children=CHILDREN)
 
         # Mock running an OpenFAST analysis by creating an empty output file.
-        with patch(
-            "openfast.routines.run_subprocess_and_log_stdout_and_stderr",
-            lambda command, logger: Path(os.path.splitext(command[1])[0] + ".out").touch(),
-        ):
+        with patch("openfast.routines.run_subprocess_and_log_stdout_and_stderr", self._create_mock_output_file):
             analysis = runner.run(input_manifest=input_manifest.serialise())
 
-        self.assertEqual(len(analysis.output_manifest.datasets), 1)
-        self.assertEqual(len(analysis.output_manifest.datasets["openfast"].files), 1)
+        # Test that the signed URLs for the dataset and its files work and can be used to reinstantiate the output
+        # manifest after serialisation.
+        downloaded_output_manifest = Manifest.deserialise(analysis.output_manifest.to_primitive())
+
+        # Check that the output dataset and its files can be accessed.
+        with downloaded_output_manifest.datasets["openfast"].files.one() as (datafile, f):
+            self.assertEqual(f.read(), "This is a mock OpenFAST output file.")
+
+    @staticmethod
+    def _create_mock_output_file(command, logger):
+        """Create a mock OpenFAST output file.
+
+        :param list(str) command:
+        :param logging.Logger logger:
+        :return None:
+        """
+        with open(os.path.splitext(command[1])[0] + ".out", "w") as f:
+            f.write("This is a mock OpenFAST output file.")
