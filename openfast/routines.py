446bf9120b161a68eb35388aa3d161d745c8d5d6
cortadocodes <cortado.codes@protonmail.com>
2022-04-20 15:41:55 +0100

    REF: Tidy up the main openfast analysis code

--- openfast/routines.py
+++ openfast/routines.py
@@ -30,40 +30,43 @@ DATASET_DOWNLOAD_LOCATIONS = {
 
 
 def run_openfast(analysis):
-    # Run turbulence simulation and add its output file to the analysis's input manifest.
+    """Run an OpenFAST analysis on the files in the input manifest and upload the output file to the cloud.
+
+    :param octue.resources.Analysis analysis:
+    :return None:
+    """
     run_turbsim(analysis)
 
     # Download all the datasets' files so they're available for the openfast shell app.
     with tempfile.TemporaryDirectory() as temporary_directory:
         for dataset in analysis.input_manifest.datasets.values():
-            download_location = DATASET_DOWNLOAD_LOCATIONS[dataset.name]
+            download_location = DATASET_DOWNLOAD_LOCATIONS.get(dataset.name)
 
             if not download_location:
+                logger.info("%r dataset not used.", dataset.name)
                 continue
 
             dataset.download_all_files(local_directory=os.path.join(temporary_directory, download_location))
 
+        main_openfast_input_file = analysis.input_manifest.get_dataset("openfast").files.one()
+        os.chdir(os.path.abspath(os.path.dirname(main_openfast_input_file.local_path)))
+
         logger.info("Beginning openfast analysis.")
+        run_subprocess_and_log_stdout_and_stderr(command=["openfast", main_openfast_input_file.name], logger=logger)
 
-        openfast_file = analysis.input_manifest.get_dataset("openfast").files.one()
-
-        os.chdir(os.path.abspath(os.path.dirname(openfast_file.local_path)))
-        run_subprocess_and_log_stdout_and_stderr(command=["openfast", openfast_file.name], logger=logger)
-
-        output_dataset = analysis.output_manifest.get_dataset("openfast")
-        output_dataset.path = os.path.join(temporary_directory, "openfast")
-        output_dataset.add(Datafile(path=os.path.splitext(openfast_file.local_path)[0] + ".out"))
+        output_file_path = os.path.splitext(main_openfast_input_file.local_path)[0] + ".out"
+        analysis.output_manifest.datasets["openfast"] = Dataset(path=os.path.join(temporary_directory, "openfast"))
+        analysis.output_manifest.datasets["openfast"].add(Datafile(path=output_file_path))
 
         analysis.finalise(upload_output_datasets_to=storage.path.join(OUTPUT_LOCATION, coolname.generate_slug()))
-
         logger.info("Finished openfast analysis.")
 
 
 def run_turbsim(analysis):
-    """Run turbsim to generate the input flow field, adding the output file to the turbsim dataset in the analysis's
-    input manifest.
+    """Run turbsim on the `turbsim` input dataset to generate the input flow field, then add the output file to the
+    turbsim dataset in the analysis's input manifest.
 
-    :param octue.resources.analysis.Analysis analysis:
+    :param octue.resources.Analysis analysis:
     :return None:
     """
     # answer = analysis.children["turbsim"].ask(
@@ -75,34 +78,3 @@ def run_turbsim(analysis):
     mock_output_dataset = Dataset.from_cloud("gs://openfast-aventa/testing/turbsim")
     mock_output_dataset._name = "turbsim"
     analysis.input_manifest.datasets["turbsim"] = mock_output_dataset
-
-
-def turbine_model_configuration(analysis):
-    """
-    TODO use OpenFAST python-toolbox to change wind turbine configuration files
-    """
-    pass
-
-
-# def wind_input_configuration(analysis):
-#     """
-#     Configure wind input for OpenFast
-#     Available options: CompInflow set to 1 - Uses TurbSim. 2 - External (OpenFOAM)
-#         1. TurbSim with primary .inp file
-#             1.1. User defined time series (metmast)
-#             1.2. User defined spectra series
-#             1.3. User defined profile
-#         2. TODO figure out how to hook up OpenFOAM
-#     """
-#
-#     # Lets just change Uref for now
-#     u_ref = analysis.input_values["u_ref"]
-#     turbine_model = analysis.configuration_values["turbine_model"]
-#     model_wind = os.path.join("5MW_Baseline", "Wind", "TurbSim.inp")
-#
-#     turbsim_input = FASTInputFile(
-#         os.path.join(REPOSITORY_ROOT, "data", "input", "turbine_models", turbine_model, model_wind)
-#     )
-#
-#     turbsim_input["URef"] = u_ref
-#     turbsim_input.write("TurbSim_configured.inp")
