7c5f154307b63698dc71e3d39eb5a9f8de67ec1b
cortadocodes <cortado.codes@protonmail.com>
2022-04-20 15:53:35 +0100

    OPS: Increase version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="openfast-service",
-    version="0.1.0",
+    version="0.2.0",
     py_modules=["app"],
     install_requires=[
         "octue @ https://github.com/octue/octue-sdk-python/archive/fix/miscellaneous-fixes.zip",
