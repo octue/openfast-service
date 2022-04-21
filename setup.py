7f0c643f30207440185a3489e20c40eaaaef6767
cortadocodes <cortado.codes@protonmail.com>
2022-04-21 17:45:17 +0100

    DEP: Add coolnames dependency; add cortadocodes as author

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from setuptools import setup
 setup(
     name="openfast-service",
     version="0.2.0",
+    author="cortadocodes <cortado.codes@protonmail.com>",
     py_modules=["app"],
     install_requires=[
+        "coolname>=1.1,<2",
         "octue @ https://github.com/octue/octue-sdk-python/archive/fix/miscellaneous-fixes.zip",
     ],
 )
