4a2ff093a25a2386218adce668980ec6c1466e9d
cortadocodes <cortado.codes@protonmail.com>
2022-05-03 16:04:18 +0100

    DEP: Use octue==0.23.1

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     py_modules=["app"],
     install_requires=[
         "coolname>=1.1,<2",
-        "octue @ https://github.com/octue/octue-sdk-python/archive/fix/validate-output-location-outside-of-twine.zip",
+        "octue==0.23.1",
     ],
 )
