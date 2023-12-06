e67236de5b9e66df500f9e1aec434a12a1185bb6
cortadocodes <cortado.codes@protonmail.com>
2023-12-06 12:13:32 +0000

    OPS: Increase minor version number
    skipci

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="openfast-service",
-    version="0.2.1",
+    version="0.3.0",
     author="Marcus Lugg <marcus@octue.com>",
     py_modules=["app"],
     install_requires=[
