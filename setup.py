3687a8836020371d10839bcf1595d22012fa24b6
cortadocodes <cortado.codes@protonmail.com>
2024-03-12 14:45:20 +0000

    DEP: Use latest `octue` branch

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup
 
 setup(
     name="openfast-service",
-    version="0.3.0",
+    version="0.3.1",
     author="Marcus Lugg <marcus@octue.com>",
     py_modules=["app"],
     install_requires=[
-        "coolname>=1.1,<2",
-        "octue==0.50.1",
+        "git+https://github.com/octue/octue-sdk-python.git@better-support-asynchronous-questions",
     ],
 )
