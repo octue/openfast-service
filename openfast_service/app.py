1a102ee8f96aff1bac928f129cf61d6036f6f8b1
cortadocodes <cortado.codes@protonmail.com>
2025-02-10 12:04:22 +0000

    FIX: Run `openfast` from directory of input dataset

--- openfast_service/app.py
+++ openfast_service/app.py
@@ -14,7 +14,8 @@ def run(analysis):
     :return None:
     """
     # Download the dataset's files so they're available for the OpenFAST CLI.
-    analysis.input_manifest.download()
+    paths = analysis.input_manifest.download()
+    os.chdir(paths["openfast"])
 
     # Get the OpenFAST entrypoint file.
     openfast_dataset = analysis.input_manifest.get_dataset("openfast")
@@ -22,15 +23,9 @@ def run(analysis):
 
     # Run the analysis.
     logger.info("Beginning OpenFAST analysis.")
-    run_logged_subprocess(command=["openfast", openfast_entry_file.local_path], logger=logger)
+    run_logged_subprocess(command=["openfast", openfast_entry_file.name], logger=logger)
     logger.info("Finished OpenFAST analysis.")
 
     # Get output file and prepare it for upload.
-    output_file_path = os.path.splitext(openfast_entry_file.local_path)[0] + ".out"
-    output_file_directory = os.path.dirname(output_file_path)
-
-    analysis.output_manifest.datasets["openfast"] = Dataset(
-        path=output_file_directory,
-        files=[output_file_path],
-        name="openfast",
-    )
+    output_file_path = os.path.splitext(openfast_entry_file.name)[0] + ".out"
+    analysis.output_manifest.datasets["openfast"] = Dataset(files=[output_file_path], name="openfast")
