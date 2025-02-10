eaac4cb4a7ce2e916a664138e5ac6960d701ba7c
cortadocodes <cortado.codes@protonmail.com>
2025-02-10 14:42:45 +0000

    TST: Update deployment test

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -1,18 +1,13 @@
 import os
 import unittest
 
-from octue.cloud import storage
 from octue.log_handlers import apply_log_handler
 from octue.resources import Child, Manifest
-
-from openfast_service import REPOSITORY_ROOT
-
 
 apply_log_handler()
 
 
-SRUID = "octue/openfast-service:0.7.0"
-DATA_DIR = os.path.join(REPOSITORY_ROOT, "tests", "data")
+SRUID = "octue/openfast-service:update-service"
 
 
 class TestDeployment(unittest.TestCase):
@@ -21,21 +16,10 @@ class TestDeployment(unittest.TestCase):
         reason="'RUN_DEPLOYMENT_TESTS' environment variable is 0 or not present.",
     )
     def test_cloud_run_deployment(self):
-        """Test that the Google Cloud Run integration works, providing a service that can be asked questions and send
+        """Test that the Google Cloud Run deployment works, providing a service that can be asked questions and send
         responses. Datasets from Google Cloud Storage are used for this test.
         """
-        input_manifest = Manifest(
-            datasets={
-                "openfast": storage.path.generate_gs_path(
-                    os.environ["TEST_BUCKET_NAME"], "openfast", "deployment_tests"
-                ),
-                "inflow": storage.path.generate_gs_path(
-                    os.environ["TEST_BUCKET_NAME"], "openfast", "deployment_tests", "inflow"
-                ),
-            },
-            ignore_stored_metadata=True,
-        )
-
+        input_manifest = Manifest(datasets={"openfast": "gs://octue-openfast-test-data/openfast_iea"})
         child = Child(id=SRUID, backend={"name": "GCPPubSubBackend", "project_name": os.environ["TEST_PROJECT_NAME"]})
 
         answer, question_uuid = child.ask(input_manifest=input_manifest, timeout=3600)
@@ -43,4 +27,4 @@ class TestDeployment(unittest.TestCase):
 
         output_dataset = answer["output_manifest"].get_dataset("openfast")
         output_files = {datafile.name: datafile for datafile in output_dataset.files}
-        self.assertEqual(output_files.keys(), {"5MW_Land_DLL_WTurb.out"})
+        self.assertEqual(output_files.keys(), {"IEA-3.4-130-RWT.outb"})
