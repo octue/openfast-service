a482a337a3a98195a92964297f76691f40dfd67c
cortadocodes <cortado.codes@protonmail.com>
2025-02-10 15:47:18 +0000

    ENH: Log which output file types aren't found

--- openfast_service/app.py
+++ openfast_service/app.py
@@ -51,6 +51,8 @@ def _prepare_output_dataset(analysis, openfast_entry_file):
         if os.path.exists(output_file_path):
             output_files.append(output_file_path)
             logger.info("%r found.", output_file_path)
+        else:
+            logger.info("No %r files found.", ext)
 
     if not output_files:
         raise ValueError(f"No output files found with extensions {POSSIBLE_OUTPUT_EXTENSIONS!r}.")
