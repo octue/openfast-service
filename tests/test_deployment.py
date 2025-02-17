7710516769d95ae7d255815cc909778abc1b19a2
cortadocodes <cortado.codes@protonmail.com>
2025-02-17 11:50:25 +0000

    OPS: Use Kueue dockerfile

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -7,7 +7,7 @@ from octue.resources import Child, Manifest
 apply_log_handler()
 
 
-SRUID = "octue/openfast-service:0.9.0"
+SRUID = "octue/openfast-service:0.10.0"
 
 
 class TestDeployment(unittest.TestCase):
