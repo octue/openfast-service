59db89ae71964fce475b94e8c0fc96beac672211
cortadocodes <cortado.codes@protonmail.com>
2025-02-18 15:55:16 +0000

    TST: Test deployment with parallel questions

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -10,13 +10,13 @@ apply_log_handler()
 SRUID = "octue/openfast-service:0.10.0"
 
 
+@unittest.skipUnless(
+    condition=os.getenv("RUN_DEPLOYMENT_TESTS", "0").lower() == "1",
+    reason="'RUN_DEPLOYMENT_TESTS' environment variable is 0 or not present.",
+)
 class TestDeployment(unittest.TestCase):
-    @unittest.skipUnless(
-        condition=os.getenv("RUN_DEPLOYMENT_TESTS", "0").lower() == "1",
-        reason="'RUN_DEPLOYMENT_TESTS' environment variable is 0 or not present.",
-    )
-    def test_cloud_run_deployment(self):
-        """Test that the Google Cloud Run deployment works, providing a service that can be asked questions and send
+    def test_single_question(self):
+        """Test that the Kubernetes/Kueue deployment works, providing a service that can be asked questions and send
         responses. Datasets from Google Cloud Storage are used for this test.
         """
         input_manifest = Manifest(datasets={"openfast": "gs://octue-openfast-test-data/openfast_iea"})
@@ -28,3 +28,18 @@ class TestDeployment(unittest.TestCase):
         output_dataset = answer["output_manifest"].get_dataset("openfast")
         output_files = {datafile.name: datafile for datafile in output_dataset.files}
         self.assertEqual(output_files.keys(), {"IEA-3.4-130-RWT.outb"})
+
+    def test_multiple_parallel_questions(self):
+        """Test that multiple parallel questions are answered correctly."""
+        number_of_questions = 50
+        input_manifest = Manifest(datasets={"openfast": "gs://octue-openfast-test-data/openfast_iea"})
+        child = Child(id=SRUID, backend={"name": "GCPPubSubBackend", "project_name": os.environ["TEST_PROJECT_NAME"]})
+
+        questions = [{"input_manifest": input_manifest, "timeout": 3600} for _ in range(number_of_questions)]
+        answers = child.ask_multiple(*questions, max_workers=50)
+
+        for answer, _ in answers:
+            self.assertEqual(len(answer["output_manifest"].datasets), 1)
+            output_dataset = answer["output_manifest"].get_dataset("openfast")
+            output_files = {datafile.name: datafile for datafile in output_dataset.files}
+            self.assertEqual(output_files.keys(), {"IEA-3.4-130-RWT.outb"})
